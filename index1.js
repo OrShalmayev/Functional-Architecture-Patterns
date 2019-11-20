080bb9104cffed81ef6f8249c9915e45c27ac1f3
classuser2 <classuser2@HQ.Taasuka.gov.il>
2019-11-20 09:20:20 +0200

    joinwithspace

--- index1.js
+++ index1.js
@@ -3,3 +3,12 @@ const fullName = (firstName, lastName) => [firstName, lastName].join(' ');
 
 fullName(user.firstName, user.lastName);
 // Output: Or Shalmayev
+
+// rename fullName to joinWithSpace
+const joinWithSpace = (...args) => args.join(' ');
+joinWithSpace(user.firstName, user.lastName);
+// Output: Or Shalmayev
+
+joinWithSpace('a','b','c'); // 'a b c'
+joinWithSpace(joinWithSpace('a','b'),'c'); // 'a b c' 
+joinWithSpace('a', joinWithSpace('b','c')); // 'a b c' 
