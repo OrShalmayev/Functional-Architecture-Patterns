5ff8b47a2efc16c67e78d4c45bf435d4f084a246
classuser2 <classuser2@HQ.Taasuka.gov.il>
2019-11-20 09:25:56 +0200

    demonstration number 3

--- index1.js
+++ index1.js
@@ -1,8 +1,12 @@
+// Demonstration Number #1
+
 const user = {firstName:"Or", lastName:"Shalmayev"};
 const fullName = (firstName, lastName) => [firstName, lastName].join(' ');
 
 fullName(user.firstName, user.lastName);
 // Output: Or Shalmayev
+
+// Demonstration Number #2
 
 // rename fullName to joinWithSpace
 const joinWithSpace = (...args) => args.join(' ');
@@ -12,3 +16,19 @@ joinWithSpace(user.firstName, user.lastName);
 joinWithSpace('a','b','c'); // 'a b c'
 joinWithSpace(joinWithSpace('a','b'),'c'); // 'a b c' 
 joinWithSpace('a', joinWithSpace('b','c')); // 'a b c' 
+
+// Demonstration Number #3
+
+class User {
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    fullName(){
+        return this.firstName + ' ' + this.lastName;
+    }
+}
+
+const newUser = new User('Or', 'Shalmayev');
+newUser.fullName(); // 'Or Shalmayev'
